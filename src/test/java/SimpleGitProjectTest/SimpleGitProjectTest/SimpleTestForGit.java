098ed1e90ee0f277ac15e79d9a96e81821afbcc0
Jega <Jega@inspiron89.home>
2023-04-23 12:38:33 -0400

    First commit by sathya

---
+++ src/test/java/SimpleGitProjectTest/SimpleGitProjectTest/SimpleTestForGit.java
@@ -0,0 +1,14 @@
+package SimpleGitProjectTest.SimpleGitProjectTest;
+
+import org.testng.annotations.Test;
+
+public class SimpleTestForGit {
+	
+	@Test
+	void firstTest() {
+		
+		System.out.println("first Program for git ");
+		
+	}
+
+}
